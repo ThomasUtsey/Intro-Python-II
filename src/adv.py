f572fa8326a64ef2a6232cdd8ec661fd244dc659
Thomas Utsey <tom@edgegluer.com>
2019-08-13 17:19:23 -0400

    * Create the REPL command parser in `adv.py` which allows the player to move to rooms   in the four cardinal directions. * Fill out Player and Room classes in `player.py` and `room.py`

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -21,23 +22,100 @@ chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
 
-
 # Link rooms together
 
-room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
+# room['outside'].n_to = room['foyer']
+# room['foyer'].s_to = room['outside']
+# room['foyer'].n_to = room['overlook']
+# room['foyer'].e_to = room['narrow']
+# room['overlook'].s_to = room['foyer']
+# room['narrow'].w_to = room['foyer']
+# room['narrow'].n_to = room['treasure']
+# room['treasure'].s_to = room['narrow']
 
 #
 # Main
 #
 
 # Make a new player object that is currently in the 'outside' room.
+character_name = input("Choose your adveturers name:")
+player = Player(character_name, room['outside'])
+print(player)
+direction = ""
+
+while player.lives == True:
+    # -----------------outside-------------------------
+
+    if player.location == room['outside']:
+        direction = input('choose a direction ( n q to quit ):')
+        if direction == "n":
+            player.location = room['foyer']
+            print(player)
+        if direction == "q":
+            print(f"{player.char_name} leaves the adventure")
+            player.lives = False
+
+    # -----------------foyer-------------------------
+
+    if player.location == room['foyer']:
+        direction = input('choose a direction ( s n e or q to quit ):')
+        if direction == "s":
+            player.location = room['outside']
+            print(player)
+        if direction == "n":
+            player.location = room['overlook']
+            print(player)
+        if direction == "e":
+            player.location = room['narrow']
+            print(player)
+        if direction == "q":
+            print(f"{player.char_name} leaves the adventure")
+            player.lives = False
+
+    # -----------------overlook-------------------------
+
+    if player.location == room['overlook']:
+        direction = input('choose a direction ( s or q to quit ):')
+        if direction == "s":
+            player.location = room['foyer']
+            print(player)
+        if direction == "q":
+            print(f"{player.char_name} leaves the adventure")
+            player.lives = False
+
+    # -----------------narrow-------------------------
+
+    if player.location == room['narrow']:
+        direction = input('choose a direction ( w, n or q to quit ):')
+        if direction == "w":
+            player.location = room['foyer']
+            print(player)
+        if direction == "n":
+            player.location = room['treasure']
+            print(player)
+        if direction == "q":
+            print(f"{player.char_name} leaves the adventure")
+            player.lives = False
+
+    # -----------------treasure-------------------------
+
+    if player.location == room['treasure']:
+        direction = input('choose a direction ( s or q to quit ):')
+        if direction == "s":
+            player.location = room['narrow']
+            print(player)
+        if direction == "q":
+            print(f"{player.char_name} leaves the adventure")
+            player.lives = False
+
+# while direction != "n" or direction != "q":
+#     direction = input('choose a direction ( n, s, e, w or q to quit ):')
+#     if direction == "n":
+#         player.location = room['foyer']
+#         print(player)fa
+#     if direction == "q":
+#         print(f"{player.char_name} leaves the adventure")
+
 
 # Write a loop that:
 #
@@ -48,4 +126,4 @@ room['treasure'].s_to = room['narrow']
 # If the user enters a cardinal direction, attempt to move to the room there.
 # Print an error message if the movement isn't allowed.
 #
-# If the user enters "q", quit the game.
+# t
